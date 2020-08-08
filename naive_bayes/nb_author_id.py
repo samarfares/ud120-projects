7aefd0a269425db7ac7620bf16565e46b7d8e62d
samar fares <samarfares1991@gmail.com>
2020-08-08 13:08:13 +0300

    Naive bayes completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,7 +13,7 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from .tools.email_preprocess import preprocess
+from email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+t0 = time()
 pred = clf.predict(features_test)
+print ("prediction time:", round(time()-t0, 3), "s")
 print (accuracy_score(labels_test, pred))
 
